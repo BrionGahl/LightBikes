573e34bef9da0c4801a58e4fbf6dc6459e5e08a8
Brion Gahl <BrionGahl@gmail.com>
2022-04-20 00:51:29 -0500

    minor change to title

--- src/main.py
+++ src/main.py
@@ -19,12 +19,26 @@ def main():
     parser.add_argument("-s", "--server", dest="server", action="store_true", help="run as host server")
     args = parser.parse_args()
         
+    print()
+    print("""
+        -------------------------------------------------------------------
+         _      _____ _____ _    _ _______   ____ _____ _  ________  _____  
+        | |    |_   _/ ____| |  | |__   __| |  _ \_   _| |/ /  ____|/ ____| 
+        | |      | || |  __| |__| |  | |    | |_) || | | ' /| |__  | (___   
+        | |      | || | |_ |  __  |  | |    |  _ < | | |  < |  __|  \___ \  
+        | |____ _| || |__| | |  | |  | |    | |_) || |_| . \| |____ ____) | 
+        |______|_____\_____|_|  |_|  |_|    |____/_____|_|\_\______|_____/ 
+        
+        ------------------------------------------------------------------- 
+        """)
+    print()    
+        
     ip = args.ip
     if args.server:
         server = threading.Thread(target=runServer, args=(), daemon=True)
         server.start()
         ip = "localhost"
-        
+            
     app = QApplication([])
     window = Window(ip)
     sys.exit(app.exec_())
